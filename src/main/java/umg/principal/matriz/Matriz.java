30aca10754f08282d62e4b0a564679d49f185e7e
mauricio9182 <pablocarrilopb0199@gmail.com>
2025-02-07 20:44:53 -0600

    Actualización del repositorio

--- src/main/java/umg/principal/matriz/Matriz.java
+++ src/main/java/umg/principal/matriz/Matriz.java
@@ -1,2 +1,29 @@
-package umg.principal.matriz;public class Matriz {
+package umg.principal.matriz;
+
+public class Matriz {
+    public static int calcularPuntuacion(int[] matriz) {
+        int puntuacion = 0;
+
+        for (int num : matriz) {
+            if (num % 2 == 0) {  // Número par (incluyendo 0)
+                puntuacion += 1;
+            } else if (num == 5) { // Número 5
+                puntuacion += 5;
+            } else { // Número impar (excepto 5)
+                puntuacion += 3;
+            }
+        }
+
+        return puntuacion;
+    }
+
+    public static void main(String[] args) {
+        int[] matriz1 = {5, 6, 7, 8, 9};
+        int[] matriz2 = {20, 22, 21};
+        int[] matriz3 = {4, 5, 5, 5};
+
+        System.out.println("Producción: " + calcularPuntuacion(matriz1)); // 13
+        System.out.println("Producción: " + calcularPuntuacion(matriz2)); // 9
+        System.out.println("Producción: " + calcularPuntuacion(matriz3)); // 15
+    }
 }
--- src/main/java/umg/principal/tribonacci/Tribonacci.java
+++ src/main/java/umg/principal/tribonacci/Tribonacci.java
@@ -1,2 +1,32 @@
-package umg.principal.tribonacci;public class Tribonacci {
+package umg.principal.tribonacci;
+
+import java.util.Arrays;
+
+public class Tribonacci {
+    public static int[] generarTribonacci(int[] firma, int n) {
+        if (n == 0) {
+            return new int[0]; //
+        }
+
+        if (n <= 3) {
+            return Arrays.copyOf(firma, n);
+        }
+
+        int[] secuencia = new int[n];
+        System.arraycopy(firma, 0, secuencia, 0, 3);
+
+        for (int i = 3; i < n; i++) {
+            secuencia[i] = secuencia[i - 1] + secuencia[i - 2] + secuencia[i - 3];
+        }
+
+        return secuencia;
+    }
+
+    public static void main(String[] args) {
+        int[] firma = {0, 1, 1};
+        int n = 9;
+
+        int[] resultado = generarTribonacci(firma, n);
+        System.out.println(Arrays.toString(resultado));
+    }
 }
